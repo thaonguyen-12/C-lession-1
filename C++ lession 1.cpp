7912491460bc56f51adc753d59065ed6ac066c53
thao nguyen <phong5363@gmail.com>
2025-08-10 13:36:17 +0700

    c++ bài 1

---
+++ C++ lession 1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+struct hinh_chu_nhat_s
+{
+    int dai;
+    int rong;
+};
+
+
+class hinh_chu_nhat_c {
+public:
+    int dai;
+    int rong;
+
+    int dien_tich() {
+        return dai * rong;
+    }
+};
+
+int main()
+{
+    hinh_chu_nhat_s A;
+    A.dai = 10;
+    A.rong = 20; 
+
+    hinh_chu_nhat_c B;
+    B.dai = 10; 
+    B.rong = 20; 
+    int x = B.dien_tich();
+    cout << "Dien tich: "<<  x << endl; 
+    return 0;
+}
+
